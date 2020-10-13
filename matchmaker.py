bb7df03cf9823e9cec05c7dd3ef2dd0856949129
Glen Crawford <glencraw4d@gmail.com>
2020-10-13 23:59:54 +1100

    Continue preprocessing data set: diet, drinks, drugs, and do a quick proof-of-concept KNN implementation with scikit-learn.

--- matchmaker.py
+++ matchmaker.py
@@ -1,12 +1,31 @@
 from matchmaker import *
 
+from sklearn.neighbors import NearestNeighbors
+
 def main():
   data_frame = DataPreprocessing.load_input_data()
   data_frame = DataPreprocessing.preprocess_input_data(data_frame)
 
-  # print(data_frame['body_type'].describe())
-  # print(data_frame['body_type'].value_counts(dropna = False))
-  print(data_frame)
+  # print(data_frame['drugs'].describe())
+  # print(data_frame['drugs'].value_counts(dropna = False))
+  # print(data_frame)
+
+  # Quick proof-of-concept testing of scikit-learn KNN.
+  # data_frame.drop(columns=['relationship_status', 'sex', 'sexual_orientation'], inplace = True)
+  data_frame = data_frame[['age']]
+  random_row = data_frame.sample()
+  index_of_random_row = random_row.iloc[[0]].index.item()
+  data_frame.drop(index_of_random_row, inplace = True)
+  print('Data frame:\n', data_frame, '\n\n')
+  print('Random row:\n', random_row, '\n\n')
+
+  # https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.NearestNeighbors.html#sklearn.neighbors.NearestNeighbors
+  nearest_neighbors_model = NearestNeighbors(n_neighbors = 5).fit(data_frame)
+  nearest_neighbors_distances, nearest_neighbors_indices = nearest_neighbors_model.kneighbors(random_row)
+
+  print('Nearest Neighbors indices:\n', nearest_neighbors_indices[0], '\n\n')
+  print('Nearest Neighbors distances:\n', nearest_neighbors_distances[0], '\n\n')
+  print('Nearest Neighbors:\n\n', data_frame.loc[nearest_neighbors_indices[0]])
 
 if __name__ == '__main__':
   main()
--- matchmaker/data_preprocessing.py
+++ matchmaker/data_preprocessing.py
@@ -12,43 +12,36 @@ INPUT_DATA_COLUMN_NAMES = [
 ]
 
 # TODO:
-# diet
-# drinks
-# drugs
 # education
 # ethnicity
 # height
-# income
 # job
-# last_online
 # location
 # offspring
 # pets
 # religion
-# sign
 # smokes
 # speaks
-# essay0
-# essay1
-# essay2
-# essay3
-# essay4
-# essay5
-# essay6
-# essay7
-# essay8
-# essay9
 
 INPUT_DATA_COLUMNS_TO_USE = [
   'age', # Numeric, min: 18, max: 110, mean: 32.3, std: 9.4, no missing values.
   'relationship_status', # String, no missing values, 5 unique values: single (93%), seeing someone (3%), available (3%), married (0.5%), unknown (0.02%). Don't one-hot encode this, pass this through exactly as we're likely going to do a direct value look-up.
   'sex', # String, no missing values, 2 unique values: m (59.8%), f (40.2%). Don't one-hot encode this, pass this through exactly as we're likely going to do a direct value look-up.
   'sexual_orientation', # String, no missing values, 3 unique values: straight (86%), gay (9.3%), bisexual (4.6%). Don't one-hot encode this, pass this through exactly as we're likely going to do a direct value look-up.
-  'body_type' # String, 5,293 missing values, 12 unique values: average, athletic, etc. Consolidate similar values to reduce uniquess values (and replace missing ones) to fit, average, curvy, thin, overweight, and unknown. And one-hot encode.
+  'body_type', # String, 5,293 missing values, 12 unique values: average, athletic, etc. Consolidate similar values to reduce unique values (and replace missing ones) to fit, average, curvy, thin, overweight, and unknown. And one-hot encode.
+  'diet', # String, 24,389 missing values (41%), 18 unique values: anything, kosher, etc. Consolidate similar values to reduce unique values (and replace missing ones) to anything, vegetarian, kosher, etc. And one-hot encode.
+  'drinks', # String, 2,985 missing values (5%), 6 unique values: often, rarely, etc. Consolidate similar values to reduce unique values (and replace missing ones). And one-hot encode.
+  'drugs' # String, 14,076 missing values (23%), 3 unique values: never, sometimes, often. Replace missing values with never, which is not a great assumption, but whatever. And one-hot encode.
 ]
 
-# NUMERIC_COLUMNS_TO_SCALE = []
-CATEGORICAL_COLUMNS_TO_ONE_HOT_ENCODE = ['body_type']
+# Input data columns not using:
+# * essay0, essay1, essay2, essay3, essay4, essay5, essay6, essay7, essay8, essay9: Skip these for now, but intend to use later, by layering some NLP similarity on top of the more basic features.
+# * last_online, sign: Not useful.
+# * income: Would be interesting, but it's apparently an optional field: 81% of all rows have a value of -1. Not enough to be useful.
+# * ?: ?
+
+NUMERIC_COLUMNS_TO_SCALE = ['age']
+CATEGORICAL_COLUMNS_TO_ONE_HOT_ENCODE = ['body_type', 'diet', 'drinks', 'drugs']
 
 def load_input_data():
   return pd.read_csv(
@@ -74,6 +67,30 @@ def preprocess_input_data(data_frame):
         'rather not say': 'unknown',
         'used up': 'unknown', # I don't even know what this means...
         np.nan: 'unknown'
+      },
+      'diet': {
+        'mostly anything': 'anything',
+        'strictly anything': 'anything',
+        'mostly vegetarian': 'vegetarian',
+        'strictly vegetarian': 'vegetarian',
+        'mostly vegan': 'vegan',
+        'strictly vegan': 'vegan',
+        'mostly kosher': 'kosher',
+        'strictly kosher': 'kosher',
+        'mostly halal': 'halal',
+        'strictly halal': 'halal',
+        'mostly other': 'other',
+        'strictly other': 'other',
+        np.nan: 'anything'
+      },
+      'drinks': {
+        'very often': 'often',
+        'not at all': 'never',
+        'desperately': 'often', # I presume this means often.
+        np.nan: 'unknown'
+      },
+      'drugs': {
+        np.nan: 'never'
       }
     }
   )
@@ -84,5 +101,9 @@ def preprocess_input_data(data_frame):
     columns = CATEGORICAL_COLUMNS_TO_ONE_HOT_ENCODE,
     sparse = False
   )
+
+  # Scale/normalize numeric columns to between 0 and 1.
+  numeric_scaler = sklearn.preprocessing.MinMaxScaler(feature_range = (0, 1), copy = True)
+  data_frame[NUMERIC_COLUMNS_TO_SCALE] = numeric_scaler.fit_transform(data_frame[NUMERIC_COLUMNS_TO_SCALE].to_numpy())
 
   return data_frame
