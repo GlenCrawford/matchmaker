c5742ad1eaa71a0f10bc0279df62a6d591be28bb
Glen Crawford <glencraw4d@gmail.com>
2020-10-16 23:56:20 +1100

    Lots of work on tidying and documenting feature engineering. Also drop married people who shouldn't be online dating.

--- matchmaker.py
+++ matchmaker.py
@@ -16,7 +16,8 @@ def main():
   data_frame.drop(index_of_random_row, inplace = True)
 
   # Get X most similar rows (nearest neighbors).
-  # By default, return the distances of all rows. This will be quite inefficient, so override this when looking for a smaller subset by specifying a smaller number when invoking #kneighbors.
+  # By default, return the distances of all rows. This will be quite inefficient, so override this when looking for a
+  # smaller subset by specifying a smaller number when invoking .kneighbors.
   # A formula of "minkowski" and p of 2 makes for a Euclidean distance metric.
   # https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.NearestNeighbors.html
   nearest_neighbors_model = NearestNeighbors(
@@ -26,7 +27,10 @@ def main():
     p = 2
   ).fit(data_frame)
   all_distances, all_indices = nearest_neighbors_model.kneighbors(random_row)
-  nearest_neighbors_distances, nearest_neighbors_indices = nearest_neighbors_model.kneighbors(random_row, n_neighbors = 5)
+  nearest_neighbors_distances, nearest_neighbors_indices = nearest_neighbors_model.kneighbors(
+    random_row,
+    n_neighbors = 5
+  )
 
   data_frame = Utilities.reverse_one_hot_encoding(data_frame)
   random_row = Utilities.reverse_one_hot_encoding(random_row)
@@ -36,7 +40,7 @@ def main():
   # within the population, converted to a percentage/score.
   nearest_neighbors_similarity_score = [100 - stats.percentileofscore(all_distances[0], distance, 'rank') for distance in nearest_neighbors_distances[0]]
 
-  # Map the neighbors' result set indices to actual rows.
+  # Fetch the neighbors' from the data set.
   nearest_neighbors = data_frame.iloc[nearest_neighbors_indices[0]]
 
   print('Random row:\n', random_row, '\n\n')
--- matchmaker/data_preprocessing.py
+++ matchmaker/data_preprocessing.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import sklearn
 from sklearn import preprocessing
 
-# Relative from the root directory.
+# Relative from the project root directory.
 INPUT_DATA_PATH = 'data/okcupid_profiles.csv'
 
 INPUT_DATA_COLUMN_NAMES = [
@@ -14,43 +14,182 @@ INPUT_DATA_COLUMN_NAMES = [
 ]
 
 INPUT_DATA_COLUMNS_TO_USE = [
-  'age', # Numeric, min: 18, max: 110, mean: 32.3, std: 9.4, no missing values.
-  'relationship_status', # String, no missing values, 5 unique values: single (93%), seeing someone (3%), available (3%), married (0.5%), unknown (0.02%). Don't one-hot encode this, pass this through exactly as we're likely going to do a direct value look-up.
-  'sex', # String, no missing values, 2 unique values: m (59.8%), f (40.2%). Don't one-hot encode this, pass this through exactly as we're likely going to do a direct value look-up.
-  'sexual_orientation', # String, no missing values, 3 unique values: straight (86%), gay (9.3%), bisexual (4.6%). Don't one-hot encode this, pass this through exactly as we're likely going to do a direct value look-up.
-  'body_type', # String, 5,293 missing values, 12 unique values: average, athletic, etc. Consolidate similar values to reduce unique values (and replace missing ones) to fit, average, curvy, thin, overweight, and unknown. And one-hot encode.
-  'diet', # String, 24,389 missing values (41%), 18 unique values: anything, kosher, etc. Consolidate similar values to reduce unique values (and replace missing ones) to anything, vegetarian, kosher, etc. And one-hot encode.
-  'drinks', # String, 2,985 missing values (5%), 6 unique values: often, rarely, etc. Consolidate similar values to reduce unique values (and replace missing ones). And one-hot encode.
-  'drugs', # String, 14,076 missing values (23%), 3 unique values: never, sometimes, often. Replace missing values with never, which is not a great assumption, but whatever. And one-hot encode.
-  'education', # String, 6,625 missing values (11%), 32 unique values: high school, college/university, etc. Consolidate similar values to reduce unique values (and replace missing ones). And one-hot encode.
-  'ethnicity', # String, 5,676 missing values (9.5%), 217 unique values: white, asian, etc. Consolidate similar values to reduce unique values (and replace missing ones). 81% of all non-missing values are white, asian, hispanic/latin or black. Lump missing and everything else into unknown. Controversial... And one-hot encode.
-  'smokes', # String. 5,511 missing values (9.2%), 5 unique values: no, sometimes, etc. Map the values to binary 0 and 1, where any degree of smoking is 1.
-  'speaks', # String, 50 missing values (~0%), 7,643 unique values: english, etc. Consolidate similar values to most common ones to reduce unique values (and replace missing ones), and trim to one each, prioritizing the first mentioned one. And one-hot encode.
-  'offspring', # String, 35,554 missing values (59.3%), 15 unique values: has kids, wants kids, etc. Consolidate similar values to reduce unique values (and replace missing ones). And one-hot encode.
-  'pets', # String, 19,916 missing values (33.2%), 15 unique values: has dogs, likes cats, etc. Consolidate similar values to reduce unique values (and replace missing ones). And one-hot encode.
-  'religion' # String, 20,221 missing values (33.7%), 45 unique values: christianity, judaism, etc. Consolidate similar values to reduce unique values (and replace missing ones). And one-hot encode.
+  # age:
+  # Numeric
+  # No missing values
+  # Minimum: 18, maximum: 110, mean: 32.3, std: 9.4
+  'age',
+
+  # relationship_status:
+  # String
+  # No missing values
+  # 5 unique values: single (93%), seeing someone (3%), available (3%), married (0.5%), unknown (0.02%)
+  # Drop rows that are unknown, and it's kind of critical given the problem domain.
+  # Drop rows that are seeing someone or married, because...why are they online dating?
+  # Don't encode, pass this through exactly as we're likely going to do a direct value look-up.
+  'relationship_status',
+
+  # sex:
+  # String
+  # No missing values
+  # 2 unique values: m (59.8%), f (40.2%)
+  # Don't encode, pass this through exactly as we're likely going to do a direct value look-up.
+  'sex',
+
+  # sexual_orientation:
+  # String
+  # No missing values
+  # 3 unique values: straight (86%), gay (9.3%), bisexual (4.6%)
+  # Don't encode, pass this through exactly as we're likely going to do a direct value look-up.
+  'sexual_orientation',
+
+  # body_type:
+  # String
+  # 5,293 missing values (8.8%)
+  # 12 unique values: average, athletic, etc.
+  # Consolidate similar values to reduce unique values to fit, average, curvy, thin, overweight, and unknown.
+  # Replace missing values with unknown.
+  # One-hot encode.
+  'body_type',
+
+  # diet:
+  # String
+  # 24,389 missing values (41%)
+  # 18 unique values: anything, kosher, etc.
+  # Consolidate similar values to reduce unique values to anything, vegetarian, kosher, etc.
+  # Replace missing values with anything.
+  # One-hot encode.
+  'diet',
+
+  # drinks:
+  # String
+  # 2,985 missing values (5%)
+  # 6 unique values: often, rarely, etc.
+  # Consolidate similar values to reduce unique values (e.g. very often to often).
+  # Replace missing values with unknown.
+  # One-hot encode.
+  'drinks',
+
+  # drugs:
+  # String
+  # 14,076 missing values (23%)
+  # 3 unique values: never, sometimes, often.
+  # Replace missing values with never, which is not a great assumption, but whatever.
+  # One-hot encode.
+  'drugs',
+
+  # education:
+  # String
+  # 6,625 missing values (11%)
+  # 32 unique values: high school, college/university, etc.
+  # Consolidate similar values to reduce unique values.
+  # Replace missing values with unknown.
+  # One-hot encode.
+  'education',
+
+  # ethnicity:
+  # String
+  # 5,676 missing values (9.5%)
+  # 217 unique values: white, asian, etc.
+  # Consolidate similar values to reduce unique values. 81% of all non-missing values are white, asian, hispanic/latin
+  # or black. Lump everything else into unknown. Controversial...
+  # Replace missing values with unknown.
+  # One-hot encode.
+  'ethnicity',
+
+  # smokes:
+  # String
+  # 5,511 missing values (9.2%)
+  # 5 unique values: no, sometimes, etc.
+  # Map the values to binary 0 and 1, where any degree of smoking is 1.
+  'smokes',
+
+  # speaks:
+  # String
+  # 50 missing values (~0%)
+  # 7,643 unique values: english, etc.
+  # Consolidate similar values to the 9 most common ones to reduce unique values, and trim to one language each,
+  # prioritizing the first mentioned one, on the (I believe correct) assumption that it's their strongest one.
+  # Replace missing values with unknown.
+  # One-hot encode.
+  'speaks',
+
+  # offspring:
+  # String
+  # 35,554 missing values (59.3%)
+  # 15 unique values: has kids, wants kids, etc.
+  # Consolidate similar values to reduce unique values down to just no_kids, no_kids_dont_want_any, has_kids,
+  # has_kids_but_no_more.
+  # replace missing values with unknown.
+  # One-hot encode.
+  'offspring',
+
+  # pets:
+  # String
+  # 19,916 missing values (33.2%)
+  # 15 unique values: has dogs, likes cats, etc.
+  # This is a strange one; it only includes cats and dogs. So if you have a rabbit but want a dog, you might be NaN, or
+  # "likes dogs", and no mention of the rabbit. All I can really do is lump anything other than has cats and/or dogs
+  # together into unknown.
+  # Consolidate similar values to reduce unique values down to just unknown, has_cats, has_dogs, has_cats_and_dogs.
+  # Replace missing values with unknown.
+  # One-hot encode.
+  'pets',
+
+  # religion:
+  # String
+  # 20,221 missing values (33.7%)
+  # 45 unique values: christianity, judaism, etc.
+  # Consolidate similar values to reduce unique values down to just atheism, agnosticism, buddhism, hinduism, islam,
+  # judaism, christianity, catholicism, other.
+  # Replace missing values with unknown.
+  # One-hot encode.
+  'religion'
 ]
 
-# In the future, consider replacing the following fields which are currently one-hot encoded into continuous values between 0 and 1.
+# Future feature engineering work:
+#
+# Consider replacing the following fields which are currently one-hot encoded into continuous values between 0 and 1.
 # * body_type
 # * diet
 # * drinks
 # * drugs
 # * education
 # * offspring
-
-# Also, in the future, consider splitting pets up into two separate features: cats and dogs.
+#
+# Also consider splitting pets up into two separate features: cats and dogs.
+#
+# age and smokes are retained as continuous and scaled between 0 and 1. Investigate whether these are appropriate scales
+# regarding distance weighting.
 
 # Input data columns not using:
-# * essay0, essay1, essay2, essay3, essay4, essay5, essay6, essay7, essay8, essay9: Skip these for now, but intend to use later, by layering some NLP similarity on top of the more basic features.
+# * essay0, essay1, essay2, essay3, essay4, essay5, essay6, essay7, essay8, essay9: Skip these for now, but intend to
+#   use them later, possibly by layering some NLP similarity on top of the more basic features.
 # * last_online, sign: Not useful.
-# * income: Would be interesting, but it's apparently an optional field: 81% of all rows have a value of -1. Not enough to be useful.
-# * height: Not really relevant for matchmaking. Is also going to correlate with sex and create noise in that sense. *Could* be untangled, as an academic exercise, but again, it's not relevant enough.
-# * location: The population of this dataset is entirely in and around San Francisco, California, US. There are 199 unique values, such as "san francisco, california". Going to drop this and pretend that it's a local dataset. Not interested in doing anything location matching/radius/etc for the purposes of this project.
-# * job: This is more like industry (e.g. entertainment, banking, etc). There are also a lot of missing, "other", etc values. Not particularly relevant for this purpose; lawyers aren't necessarily looking to date other lawyers. Might do something with this later on though.
+# * income: Would be interesting, but it's apparently an optional field: 81% of all rows have a value of -1. Not enough
+#   to be useful.
+# * height: Not really relevant for matchmaking. Is also going to correlate with sex and create noise in that sense.
+#   *Could* be untangled, as an academic exercise, but again, it's not relevant enough.
+# * location: The population of this dataset is entirely in and around San Francisco, California, US. There are 199
+#   unique values, such as "san francisco, california". Going to drop this and pretend that it's a local dataset. Not
+#   interested in doing anything regarding location matching/radius/etc for the purposes of this project.
+# * job: This is more like industry (e.g. entertainment, banking, etc). There are also a lot of missing "other", etc
+#   values. Not particularly relevant for this purpose; lawyers aren't necessarily looking to date other lawyers. Might
+#   do something with this later on though.
 
-NUMERIC_COLUMNS_TO_SCALE = ['age']
-CATEGORICAL_COLUMNS_TO_ONE_HOT_ENCODE = ['body_type', 'diet', 'drinks', 'drugs', 'education', 'ethnicity', 'speaks', 'offspring', 'pets', 'religion']
+CONTINUOUS_COLUMNS_TO_SCALE = ['age']
+CATEGORICAL_COLUMNS_TO_ONE_HOT_ENCODE = [
+  'body_type',
+  'diet',
+  'drinks',
+  'drugs',
+  'education',
+  'ethnicity',
+  'speaks',
+  'offspring',
+  'pets',
+  'religion'
+]
 
 def load_input_data():
   return pd.read_csv(
@@ -61,10 +200,12 @@ def load_input_data():
   )
 
 def preprocess_input_data(data_frame):
-  # Drop rows where relationship_status = unknown.
-  data_frame = data_frame[data_frame['relationship_status'] != 'unknown']
+  # Drop rows where relationship_status is unknown.
+  # Also drop rows that are seeing someone or married. Get off OkCupid.
+  relationship_statuses_to_drop = ['unknown', 'seeing someone', 'married']
+  data_frame = data_frame.drop(data_frame[data_frame['relationship_status'].isin(relationship_statuses_to_drop)].index)
 
-  # Consolidate similar body types from 12 unique values (plus missing) to 6.
+  # Per-feature value consolidation.
   data_frame = data_frame.replace(
     {
       'body_type': {
@@ -146,7 +287,7 @@ def preprocess_input_data(data_frame):
         'when drinking': 1,
         'yes': 1,
         'trying to quit': 1,
-        np.nan: 0 # Assume no, as 81% of all non-missing values are no, so it's by far the most likely value. Very imperfect rationale though.
+        np.nan: 0 # 81% of all non-missing values are no so it's by far the most likely value. Very imperfect rationale.
       },
       'speaks': {
         np.nan: 'unknown'
@@ -169,8 +310,6 @@ def preprocess_input_data(data_frame):
         'has kids, and wants more': 'has_kids',
         np.nan: 'unknown'
       },
-      # This is a strange one, it only includes cats and dogs. So if you have a rabbit, you might be NaN, or "likes
-      # dogs". All I can really do is lump anything other than has cats and/or dogs together into unknown.
       'pets': {
         'dislikes dogs and dislikes cats': 'unknown',
         'dislikes cats': 'unknown',
@@ -197,7 +336,7 @@ def preprocess_input_data(data_frame):
         'atheism and laughing about it': 'atheism',
         'agnosticism but not too serious about it': 'agnosticism',
         'agnosticism and somewhat serious about it': 'agnosticism',
-        'agnosticism and very serious about it': 'agnosticism',
+        'agnosticism and very serious about it': 'agnosticism', # What does it even mean to be a very serious agnostic?
         'agnosticism and laughing about it': 'agnosticism',
         'buddhism but not too serious about it': 'buddhism',
         'buddhism and somewhat serious about it': 'buddhism',
@@ -232,12 +371,15 @@ def preprocess_input_data(data_frame):
     }
   )
 
+  # Regular expression value consolidations.
   data_frame = data_frame.replace(
     {
+      # I am *not* mapping 217 unique values!
       'ethnicity': {
-        r'^(?!white$|asian$|hispanic_latin$|black$).*': 'unknown' # I am *not* mapping 217 unique values...
+        r'^(?!white$|asian$|hispanic_latin$|black$).*': 'unknown'
       },
-      # Believe it or not, this is enough to consolidate and trim down to each row having one normalized language (favouring their first listed) without any non-whitelisted values.
+      # Believe it or not, this is enough to consolidate and trim down to each row having one normalized language
+      # (favouring their first listed).
       'speaks': {
         r'^mandarin(.*)$': 'mandarin_chinese',
         r'^spanish(.*)$': 'spanish',
@@ -260,8 +402,10 @@ def preprocess_input_data(data_frame):
     sparse = False
   )
 
-  # Scale/normalize numeric columns to between 0 and 1.
-  numeric_scaler = sklearn.preprocessing.MinMaxScaler(feature_range = (0, 1), copy = True)
-  data_frame[NUMERIC_COLUMNS_TO_SCALE] = numeric_scaler.fit_transform(data_frame[NUMERIC_COLUMNS_TO_SCALE].to_numpy())
+  # Linearly scale/normalize continuous features.
+  continuous_scaler = sklearn.preprocessing.MinMaxScaler(feature_range = (0, 1))
+  data_frame[CONTINUOUS_COLUMNS_TO_SCALE] = continuous_scaler.fit_transform(
+    data_frame[CONTINUOUS_COLUMNS_TO_SCALE].to_numpy()
+  )
 
   return data_frame
