9bce31ffeb5d779ca14b2c91bfab73328dd75663
Glen Crawford <glencraw4d@gmail.com>
2020-10-17 22:05:01 +1100

    Run inference only once by getting the nearest neighbors from the all results, not a second run.

--- matchmaker.py
+++ matchmaker.py
@@ -2,6 +2,8 @@ from matchmaker import *
 from sklearn.neighbors import NearestNeighbors
 from scipy import stats
 
+NEAREST_NEIGHBORS_TO_RETRIEVE = 40
+
 def main():
   population_data_frame = DataPreprocessing.load_input_data()
   population_data_frame = add_input_data_to_population(population_data_frame)
@@ -25,16 +27,16 @@ def main():
     p = 2
   ).fit(population_data_frame.loc[:, ~population_data_frame.columns.isin(DataPreprocessing.DIRECT_LOOKUP_FEATURES)])
 
-  # Can these both be done at once?
   all_distances, all_indices = nearest_neighbors_model.kneighbors(
     input_data_frame.loc[:, ~input_data_frame.columns.isin(DataPreprocessing.DIRECT_LOOKUP_FEATURES)]
   )
 
-  nearest_neighbors_distances, nearest_neighbors_indices = nearest_neighbors_model.kneighbors(
-    input_data_frame.loc[:, ~input_data_frame.columns.isin(DataPreprocessing.DIRECT_LOOKUP_FEATURES)],
-    n_neighbors = 20
-  )
+  # We did inference for all rows, now extract out the X number of ones that we want. The returned indices and distances
+  # are sorted by distance in ascending order, so nearest first.
+  nearest_neighbors_distances = [all_distances[0][:NEAREST_NEIGHBORS_TO_RETRIEVE]]
+  nearest_neighbors_indices = [all_indices[0][:NEAREST_NEIGHBORS_TO_RETRIEVE]]
 
+  # Reverse some of the data preprocessing to make the data frame prettier for output.
   population_data_frame = Utilities.reverse_one_hot_encoding(population_data_frame)
   input_data_frame = Utilities.reverse_one_hot_encoding(input_data_frame)
 
@@ -52,11 +54,10 @@ def main():
   # Fetch the neighbors' from the population data frame.
   nearest_neighbors_data_frame = population_data_frame.iloc[nearest_neighbors_indices[0]]
 
+  # Zip the similarity scores into the nearest neighbors as the first column.
+  nearest_neighbors_data_frame.insert(loc = 0, column = 'score', value = nearest_neighbors_similarity_score)
+
   print('Input data:\n', input_data_frame, '\n\n')
-  print('All distances (closer to zero the better):\n', all_distances[0], '\n\n')
-  print('Nearest Neighbors indices (within the results, not initial data set):\n', nearest_neighbors_indices[0], '\n\n')
-  print('Nearest Neighbors distances (closer to zero the better):\n', nearest_neighbors_distances[0], '\n\n')
-  print('Nearest Neighbors similarity score (higher the better):\n', nearest_neighbors_similarity_score, '\n\n')
   print('Nearest Neighbors:\n', nearest_neighbors_data_frame)
 
 def add_input_data_to_population(population_data_frame):
