386a5dc7a028fc9148aff9775cbaf8eec6556059
Glen Crawford <glencraw4d@gmail.com>
2020-10-18 23:56:10 +1100

    Refactor how distances and indices from the nearest neighbors model are converted into results so that the model can be trained on the entire population rather than just the filtered candidates for a single input.

--- matchmaker.py
+++ matchmaker.py
@@ -13,15 +13,16 @@ def main():
   input_data_frame = population_data_frame.loc[['input']]
   population_data_frame.drop('input', inplace = True)
 
+  # For testing/debugging preprocessing:
   # print(population_data_frame['speaks'].value_counts(dropna = False))
   # print(population_data_frame.filter(regex = r'^drinks(.*)$', axis = 1))
   #
   # exit()
 
-  population_data_frame = apply_direct_lookups(input_data_frame, population_data_frame)
+  candidates_data_frame = apply_direct_lookups(input_data_frame, population_data_frame)
 
-  # If there is no population to search for similarity within after applying the direct lookups, stop here.
-  if len(population_data_frame) == 0:
+  # If there are no candidates to search for similarity within after applying the direct lookups, stop here.
+  if len(candidates_data_frame) == 0:
     print('No results :(')
     exit()
 
@@ -37,35 +38,58 @@ def main():
     p = 2
   ).fit(population_data_frame.loc[:, ~population_data_frame.columns.isin(DataPreprocessing.DIRECT_LOOKUP_FEATURES)])
 
-  all_distances, all_indices = nearest_neighbors_model.kneighbors(
+  population_distances, population_indices = nearest_neighbors_model.kneighbors(
     input_data_frame.loc[:, ~input_data_frame.columns.isin(DataPreprocessing.DIRECT_LOOKUP_FEATURES)]
   )
 
-  # We did inference for all rows, now extract out the X number of ones that we want. The returned indices and distances
-  # are sorted by distance in ascending order, so nearest first.
-  nearest_neighbors_distances = [all_distances[0][:NEAREST_NEIGHBORS_TO_RETRIEVE]]
-  nearest_neighbors_indices = [all_indices[0][:NEAREST_NEIGHBORS_TO_RETRIEVE]]
+  population_indices = population_indices[0]
+  population_distances = population_distances[0]
 
-  # Reverse some of the data preprocessing to make the data frame prettier for output.
-  population_data_frame = Utilities.reverse_one_hot_encoding(population_data_frame)
-  input_data_frame = Utilities.reverse_one_hot_encoding(input_data_frame)
+  # We did inference for the entire population, now we need to reduce that down to only the candidates and filter down
+  # to the X number of results that we want.
+  #
+  # Note that population_indices are the indices (not labels) of the row in the population data frame on which the model
+  # was fitted.
+  #
+  # The returned indices and distances are sorted by distance in ascending order, so nearest first.
 
-  population_data_frame = Utilities.reverse_continuous_scaling(population_data_frame)
-  input_data_frame = Utilities.reverse_continuous_scaling(input_data_frame)
+  # Reduce the population indices and distances down to only those that are candidates.
+  candidates_indices = []
+  candidates_distances = []
 
-  population_data_frame = Utilities.reverse_ordinal_encoding(population_data_frame)
-  input_data_frame = Utilities.reverse_ordinal_encoding(input_data_frame)
+  for index, population_index in enumerate(population_indices):
+    population_distance = population_distances[index]
+
+    population_label = population_data_frame.index[population_index]
+
+    if population_label in candidates_data_frame.index:
+      candidates_indices.append(population_index)
+      candidates_distances.append(population_distance)
+
+  # Slice out only the desired number of candidates.
+  nearest_neighbors_indices = candidates_indices[:NEAREST_NEIGHBORS_TO_RETRIEVE]
+  nearest_neighbors_distances = candidates_distances[:NEAREST_NEIGHBORS_TO_RETRIEVE]
 
   # Calculate a "similarity score" of each neighbor. This is not the absolute similarity of the neighbor from the target
-  # one, it's more of its percentile ranking within the distances of all rows, meaning that it's effectively its ranking
-  # within the population, converted to a percentage/score.
+  # one, it's more of its percentile ranking within the distances of all candidates, meaning that it's effectively its
+  # ranking within the candidates, converted to a percentage/score.
   nearest_neighbors_similarity_score = [
-    round((100 - stats.percentileofscore(all_distances[0], distance, 'rank')), 2)
-    for distance in nearest_neighbors_distances[0]
+    round((100 - stats.percentileofscore(candidates_distances, distance, 'rank')), 2)
+    for distance in nearest_neighbors_distances
   ]
 
-  # Fetch the neighbors' from the population data frame.
-  nearest_neighbors_data_frame = population_data_frame.iloc[nearest_neighbors_indices[0]]
+  # Fetch the neighbors' rows from the population data frame.
+  nearest_neighbors_data_frame = population_data_frame.iloc[nearest_neighbors_indices].copy()
+
+  # Reverse some of the data preprocessing to make the data frame prettier for output.
+  input_data_frame = Utilities.reverse_one_hot_encoding(input_data_frame)
+  nearest_neighbors_data_frame = Utilities.reverse_one_hot_encoding(nearest_neighbors_data_frame)
+
+  input_data_frame = Utilities.reverse_continuous_scaling(input_data_frame)
+  nearest_neighbors_data_frame = Utilities.reverse_continuous_scaling(nearest_neighbors_data_frame)
+
+  input_data_frame = Utilities.reverse_ordinal_encoding(input_data_frame)
+  nearest_neighbors_data_frame = Utilities.reverse_ordinal_encoding(nearest_neighbors_data_frame)
 
   # Zip the similarity scores into the nearest neighbors as the first column.
   nearest_neighbors_data_frame.insert(loc = 0, column = 'score', value = nearest_neighbors_similarity_score)
@@ -104,22 +128,24 @@ def apply_direct_lookups(input_data_frame, population_data_frame):
   input_sexual_orientation = input_data_frame.iloc[0]['sexual_orientation']
   input_speaks = input_data_frame.iloc[0]['speaks']
 
+  candidates_data_frame = population_data_frame
+
   # If input is straight:
   # * Filter sex to other sex.
   # * Filter sexual_orientation to straight and bisexual.
   if input_sexual_orientation == 'straight':
-    population_data_frame = population_data_frame[
-      population_data_frame['sex'].isin([{ 'm': 'f', 'f': 'm' }[input_sex]]) &
-      population_data_frame['sexual_orientation'].isin(['straight', 'bisexual'])
+    candidates_data_frame = candidates_data_frame[
+      candidates_data_frame['sex'].isin([{ 'm': 'f', 'f': 'm' }[input_sex]]) &
+      candidates_data_frame['sexual_orientation'].isin(['straight', 'bisexual'])
     ]
   # If input is gay:
   # * Filter sex to input sex.
   # * Filter sexual_orientation to gay and bisexual.
   elif input_sexual_orientation == 'gay':
-    population_data_frame = population_data_frame[
-      (population_data_frame['sex'] == input_sex)
+    candidates_data_frame = candidates_data_frame[
+      (candidates_data_frame['sex'] == input_sex)
       &
-      population_data_frame['sexual_orientation'].isin(['gay', 'bisexual'])
+      candidates_data_frame['sexual_orientation'].isin(['gay', 'bisexual'])
     ]
   # If input is bisexual:
   # * If input is male, filter:
@@ -132,22 +158,22 @@ def apply_direct_lookups(input_data_frame, population_data_frame):
   #   - (sex is female) and (sexual_orientation is gay or bisexual)
   elif input_sexual_orientation == 'bisexual':
     if input_sex == 'm':
-      population_data_frame = population_data_frame[
-        ((population_data_frame['sex'] == 'm') & population_data_frame['sexual_orientation'].isin(['gay', 'bisexual']))
+      candidates_data_frame = candidates_data_frame[
+        ((candidates_data_frame['sex'] == 'm') & candidates_data_frame['sexual_orientation'].isin(['gay', 'bisexual']))
         |
-        ((population_data_frame['sex'] == 'f') & population_data_frame['sexual_orientation'].isin(['straight', 'bisexual']))
+        ((candidates_data_frame['sex'] == 'f') & candidates_data_frame['sexual_orientation'].isin(['straight', 'bisexual']))
       ]
     elif input_sex == 'f':
-      population_data_frame = population_data_frame[
-        ((population_data_frame['sex'] == 'm') & population_data_frame['sexual_orientation'].isin(['straight', 'bisexual']))
+      candidates_data_frame = candidates_data_frame[
+        ((candidates_data_frame['sex'] == 'm') & candidates_data_frame['sexual_orientation'].isin(['straight', 'bisexual']))
         |
-        ((population_data_frame['sex'] == 'f') & population_data_frame['sexual_orientation'].isin(['gay', 'bisexual']))
+        ((candidates_data_frame['sex'] == 'f') & candidates_data_frame['sexual_orientation'].isin(['gay', 'bisexual']))
       ]
 
   # Filter by the input language.
-  population_data_frame = population_data_frame[population_data_frame['speaks'] == input_speaks]
+  candidates_data_frame = candidates_data_frame[candidates_data_frame['speaks'] == input_speaks]
 
-  return population_data_frame
+  return candidates_data_frame
 
 if __name__ == '__main__':
   main()
